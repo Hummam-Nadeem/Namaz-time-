0cbdef2c720f37e111922cf0d83146f4f10f09e2
hummam nadeem <chhummam1234@fgmail.com>
2022-07-06 23:22:16 +0500

    push

---
+++ app.js
@@ -0,0 +1,22 @@
+let data = ()=>{
+    let main = document.getElementById('main');
+    let city = document.getElementById('city').value;
+    let country = document.getElementById('country').value;
+    let form = document.getElementById('form');
+    
+    axios.get(`https://api.aladhan.com/v1/timingsByCity?city=${city}&country=""&method=8`)
+            .then(function (response) {
+                data = response.data;
+
+                
+                document.getElementById('Fajar').innerHTML = "Timing:" +"  "+ data.data.timings.Fajr;
+                document.getElementById('Zohar').innerHTML = "Timing:" +"  "+ data.data.timings.Dhuhr;
+                document.getElementById('Asar').innerHTML = "Timing:" +"  "+ data.data.timings.Asr;
+                document.getElementById('Maghrib').innerHTML = "Timing:" +"  "+ data.data.timings.Maghrib;
+                document.getElementById('Esha').innerHTML = "Timing:" +"  "+ data.data.timings.Isha;
+                // main.style.display = 'flex';
+                form.style.display = 'none';
+                
+            })
+
+        }
